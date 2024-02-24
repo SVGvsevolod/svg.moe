e915656710cfb78dfdb18adf999a7c6c49de22a0
Vsevolod Subbotkin <45977320+SVGvsevolod@users.noreply.github.com>
2024-02-24 13:04:04 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // Imports
-import { createServer } from 'node:http'
+import http from 'node:http'
+import https from 'node:https'
 import { handler } from './web/index.js'
+import { config } from 'dotenv'
+// Laoding env vars
+config()
 // Start HTTP (Web) Server
-createServer({
+http.createServer({
     keepAliveTimeout: 0
 }, handler).listen(80)
+// Start HTTPS (Web) Server
+if (process.env.ca && process.env.cert && process.env.key)
+    https.createServer({
+        ca: process.env.ca,
+        cert: process.env.cert,
+        keepAliveTimeout: 0,
+        key: process.env.key
+    }, handler).listen(443)
 // Logging process termination
 process.on('exit', a => {
     process.stdout.write(`(${a}) by\n`)
