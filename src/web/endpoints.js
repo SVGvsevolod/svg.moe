d3d0c2a3a4ef1f6bde227a80eeb3fbc93a7846b5
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2024-02-28 14:49:08 +0200

    wip

--- src/web/endpoints.js
+++ src/web/endpoints.js
@@ -5,28 +5,34 @@ export async function GET(a, b) {
         else
             switch (true) {
                 case /^\/$/.test(a.endpoint)
-                && 'object' == typeof a.headers
-                && 'string' == typeof a.headers.accept
-                && a.headers.accept.indexOf('text/html') > -1:
+                    && 'object' == typeof a.headers
+                    && 'string' == typeof a.headers.accept
+                    && a.headers.accept.indexOf('text/html') > -1:
                     b.res({
                         mime: 'html'
                     })
                     break;
                 case /^\/favicon\.ico$/.test(a.endpoint)
-                && 'object' == typeof a.headers
-                && 'string' == typeof a.headers.accept
-                && a.headers.accept.indexOf('*/*') > -1:
+                    && 'object' == typeof a.headers
+                    && 'string' == typeof a.headers.accept
+                    && a.headers.accept.indexOf('*/*') > -1:
                     b.res()
                     break;
                 case /^\/js\/sl\.js$/.test(a.endpoint)
-                && 'object' == typeof a.headers
-                && 'string' == typeof a.headers.accept
-                && a.headers.accept.indexOf('*/*') > -1:
+                    && 'object' == typeof a.headers
+                    && 'string' == typeof a.headers.accept
+                    && a.headers.accept.indexOf('*/*') > -1:
                 case /^\/js\/sl\.min\.js$/.test(a.endpoint)
-                && 'object' == typeof a.headers
-                && 'string' == typeof a.headers.accept
-                && a.headers.accept.indexOf('*/*') > -1:
+                    && 'object' == typeof a.headers
+                    && 'string' == typeof a.headers.accept
+                    && a.headers.accept.indexOf('*/*') > -1:
                     b.res()
+                    break;
+                case /.+/.test(a.endpoint)
+                    && 'object' == typeof a.headers
+                    && 'string' == typeof a.headers.accept
+                    && a.headers.accept.indexOf('text/html') > -1:
+                    b.redir(301, '/?e=404')
                     break;
                 default:
                     b.res(204)
